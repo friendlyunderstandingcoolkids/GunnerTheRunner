4b01ea616b541c03c7f18bff6afe39a04873c006
Dylan Chew <a00986529@administorsimac.ad.bcit.ca>
2019-02-16 18:38:36 -0800

    created title screen

---
+++ GunnerTheRunner/GunnerTheRunner/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  GunnerTheRunner
+//
+//  Created by Dylan Chew on 2019-02-16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
